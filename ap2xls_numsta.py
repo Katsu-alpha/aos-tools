dfc67647957614b7c30be5ffd4969d52ac7fd0d0
Katsuhiro Sasaki <git@k2hiro.com>
2024-01-10 16:30:43 +0800

    added AP name filter feature

--- ap2xls_numsta.py
+++ ap2xls_numsta.py
@@ -16,6 +16,8 @@ from openpyxl import Workbook
 from openpyxl.styles import Font, PatternFill
 from collections import defaultdict
 
+# APpat = "^APGTS"
+
 def uniq(tbl):
     ret = []
     k = set()
@@ -68,16 +70,27 @@ if __name__ == '__main__':
     print(f"{len(ap_active_tbl)-1} unique APs found in active ap table.")
 
     #
+    #   filter AP database table
+    #
+    ap_db_tbl = []
+    ap_db_tbl.append(ap_database_tbl[0])
+    for r in ap_database_tbl[1:]:
+        if 'APpat' in globals() and not re.search(APpat, r[0]):
+            continue
+        ap_db_tbl.append(r)
+
+    #
     #   AP model tally
     #
     apmodelctr = defaultdict(lambda: 0)
-    for r in ap_database_tbl[1:]:
+    for r in ap_db_tbl[1:]:
         model = r[2]
         apmodelctr[model] += 1
     print("AP models")
     for model in sorted(apmodelctr.keys()):
         print(f"{model}: {apmodelctr[model]}")
     print()
+
 
     #
     #   Process columns in active AP table
@@ -86,13 +99,16 @@ if __name__ == '__main__':
     ap_act_tbl.append(["Name", "5G PHY", "5G Ch", "5G EIRP", "5G STA", "2.4G PHY", "2.4G Ch", "2.4G EIRP", "2.4G STA"])
     idx_r0 = ap_active_tbl[0].index("Radio 0 Band Ch/EIRP/MaxEIRP/Clients")
     idx_r1 = ap_active_tbl[0].index("Radio 1 Band Ch/EIRP/MaxEIRP/Clients")
-    idx_name = ap_active_tbl[0].index("Name")
     r0chctr = defaultdict(lambda: 0)
     r0chsta = defaultdict(lambda: 0)
+    usersperfloor = defaultdict(lambda: 0)
 
     for row in ap_active_tbl[1:]:
+        if 'APpat' in globals() and not re.search(APpat, row[0]):
+            continue
+
         r0 = row[idx_r0]
-        r = re.match("(.+):([\dSE+\-]+)/([\d\.]+)/[\d\.]+/(\d+)$", r0)
+        r = re.match("(.+):([\dSE+\-]+)/([\d\.]+)/[\d\.]+/(\d+)$", r0)      # AP:5GHz-HE:124/14.0/30.0/8
         if r:
             r0_phy  = r.group(1)
             r0_ch   = r.group(2)
@@ -104,10 +120,10 @@ if __name__ == '__main__':
             r0_phy = ""
             r0_ch = ""
             r0_eirp = ""
-            r0_sta = ""
+            r0_sta = "0"
 
         r1 = row[idx_r1]
-        r = re.match("(.+):([\dSE+\-]+)/([\d\.]+)/[\d\.]+/(\d+)$", r1)
+        r = re.match("(.+):([\dSE+\-]+)/([\d\.-]+)/[\d\.-]+/(\d+)$", r1)      # AP:2.4GHz-HE:11/8.0/27.7/0
         if r:
             r1_phy  = r.group(1)
             r1_ch   = r.group(2)
@@ -117,20 +133,41 @@ if __name__ == '__main__':
             r1_phy = ""
             r1_ch = ""
             r1_eirp = ""
-            r1_sta = ""
+            r1_sta = "0"
 
 
-        ap_act_tbl.append([row[idx_name], r0_phy, r0_ch, r0_eirp, r0_sta, r1_phy, r1_ch, r1_eirp, r1_sta])
+        ap_act_tbl.append([row[0], r0_phy, r0_ch, r0_eirp, r0_sta, r1_phy, r1_ch, r1_eirp, r1_sta])
+
+        #
+        #  フロア毎のユーザ数集計
+        #
+        m = re.match(r'GTS(\d\d)', row[1])
+        if m:
+            fl = m.group(1)
+        else:
+            fl = 'n/a'
+            #print(f"Uknown floor: AP {row[0]} Group {row[1]}")
+        try:
+            usersperfloor[fl] += int(r0_sta) + int(r1_sta)
+        except Exception as e:
+            print(f"Error: {e}")
+            print(row)
+            sys.exit(-1)
 
 
     print("5GHz Channel distribution")
     for ch in sorted(r0chctr.keys()):
         print(f"{ch}: {r0chctr[ch]} ({r0chsta[ch]})")
 
+    print("\nUsers per floor")
+    for fl in sorted(usersperfloor.keys()):
+        print(f"{fl}: {usersperfloor[fl]}")
+
+
     #
     #   table join & sort
     #
-    df_ap_database = pd.DataFrame(ap_database_tbl[1:], columns=ap_database_tbl[0])
+    df_ap_database = pd.DataFrame(ap_db_tbl[1:], columns=ap_db_tbl[0])
     df_ap_act      = pd.DataFrame(ap_act_tbl[1:], columns=ap_act_tbl[0])
 
     df1 = df_ap_database.merge(df_ap_act, how="left", left_on="Name", right_on="Name")
