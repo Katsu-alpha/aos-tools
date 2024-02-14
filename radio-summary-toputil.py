044b3bbb4e865ce4f496a00182f8e87027e487de
Katsuhiro Sasaki <git@k2hiro.com>
2024-02-14 18:55:05 +0800

    save output as excel file

--- radio-summary-toputil.py
+++ radio-summary-toputil.py
@@ -11,6 +11,10 @@ import argparse
 import mylogger as log
 from aos_parser import AOSParser, AP_DATABASE_LONG_TABLE, AP_ACTIVE_TABLE
 from collections import defaultdict
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl import Workbook
+from openpyxl.styles import Font, PatternFill, Alignment
+import pandas as pd
 
 # APpat = "^APKUDKS|^APSMFTM"
 # APpat = "^APGTS"
@@ -56,7 +60,7 @@ if __name__ == '__main__':
     idx_r0 = ap_active_tbl[0].index("Radio 0 Band Ch/EIRP/MaxEIRP/Clients")
     idx_r1 = ap_active_tbl[0].index("Radio 1 Band Ch/EIRP/MaxEIRP/Clients")
     apn_ch_sta = defaultdict(lambda: {})
-    apn_type = {}
+
     for row in ap_active_tbl[1:]:
         apn = row[0]
 
@@ -64,17 +68,16 @@ if __name__ == '__main__':
         r = re.search(r":(\d+[SE+\-]?)/[\d\.]+/[\d\.]+/(\d+)$", r0)
         if r:
             ch = r.group(1)
-            nsta = r.group(2)
+            nsta = int(r.group(2))
             apn_ch_sta[apn][ch] = nsta
 
         r1 = row[idx_r1]
         r = re.search(r":(\d+[SE+\-]?)/[\d\.]+/[\d\.]+/(\d+)$", r1)
         if r:
             ch = r.group(1)
-            nsta = r.group(2)
+            nsta = int(r.group(2))
             apn_ch_sta[apn][ch] = nsta
 
-        apn_type[apn] = row[3]
 
     #
     #   radio-summary の必要な column のみ取り出す
@@ -88,6 +91,7 @@ if __name__ == '__main__':
             continue
 
         apg = r[1]
+        apt = r[2]
         band = r[4]
         if band != '5GHz':
             continue
@@ -98,11 +102,12 @@ if __name__ == '__main__':
             ch = m.group(1)
         m = re.search("([0-9\.]+)/", r[6])   # EIRP/MaxEIRP
         if m:
-            eirp = m.group(1)
-        m = re.search("(-\d+)/(\d+)/\d+", r[7])     # NF/U/I
+            eirp = float(m.group(1))
+        m = re.search("(-\d+)/(\d+)/(\d+)", r[7])     # NF/U/I
         if m:
-            nf = m.group(1)
+            nf   = int(m.group(1))
             util = int(m.group(2))
+            intf = int(m.group(3))
 
         ch_ctr[ch] += 1
         util_sum[ch] += util
@@ -110,18 +115,69 @@ if __name__ == '__main__':
             sta = apn_ch_sta[apn][ch]
         else:
             sta = "na"
-        row = [ apn, apg, mode, eirp, sta, nf, util, apn_type[apn] ]
+        row = [ apn, apg, apt, mode, eirp, sta, nf, intf, util ]
         tbl.append(row)
 
-    tbl.sort(key=lambda x: x[6], reverse=True)
+    # sort by Utilization
+    tbl.sort(key=lambda x: x[8], reverse=True)
 
     print("Name                        Group                           Type  Mode          EIRP    Clients  NF    Util")
     print("----                        -----                           ----  ----          ----    -------  ---   ----")
     for r in tbl:
-        print(f"{r[0]:28}{r[1]:32}{r[7]:6}{r[2]:14}{r[3]:>4}{r[4]:>7}  {r[5]:>7}{r[6]:>7}")
+        print(f"{r[0]:28}{r[1]:32}{r[2]:6}{r[3]:14}{r[4]:>4}{r[5]:>7}  {r[6]:>7}{r[8]:>7}")
 
-    print(f"Total APs: {len(apn_type)}")
+    print(f"Total APs: {len(apn_ch_sta)}")
     print(f"Total Radios: {len(tbl)}")
     for ch in sorted(ch_ctr.keys(), key=lambda x: int(x)):
         avg = util_sum[ch] / ch_ctr[ch]
         print(f"{ch} - {avg:.2f} ({ch_ctr[ch]} Radios)")
+
+
+    #
+    #   Excel 書き出し
+    #
+    xlsfile = "toputil.xlsx"
+
+    df_toputil = pd.DataFrame(tbl, columns=["AP Name", "Group", "Type", "Mode", "EIRP (dBm)", "Clients", "Noise (dBm)", "Intf (%)", "Util (%)"])
+
+    wb = Workbook()
+    ws = wb.active
+    for r in dataframe_to_rows(df_toputil, index=False, header=True):
+        ws.append(r)
+
+    # set font
+    f = Font(name='Consolas')
+    for row in ws.iter_rows(min_row=1):
+        for cell in row:
+            cell.font = f
+
+    # set column width
+    widths = [30, 40, 10, 20, 10, 10, 10, 10, 10]
+    for i, w in enumerate(widths):
+        ws.column_dimensions[chr(65+i)].width = w
+
+    # set header font and color
+    f = Font(name='Arial', bold=True, size=9)
+    s = PatternFill(fgColor="BDD7EE", fill_type="solid")
+    for cell in ws['A1':'I1'][0]:
+        cell.fill = s
+        cell.font = f
+
+    # centering for AP type
+    for cell in ws['C'][1:]:
+        cell.alignment = Alignment(horizontal='center')
+
+
+    # set autofilter and freeze panes
+    ws.auto_filter.ref = "A:I"
+    ws.freeze_panes = "A2"
+
+
+    #
+    #   output to file
+    #
+    print(f"Writing to {xlsfile} ... ", end="")
+    wb.save(xlsfile)
+    print("done.")
+
+    sys.exit(0)
