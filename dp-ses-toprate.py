de1eff279fb4c8ab356473d0bba7eb763dbb1654
Katsuhiro Sasaki <git@k2hiro.com>
2025-09-11 00:44:20 +0800

    support --pattern and --top options

--- dp-ses-toprate.py
+++ dp-ses-toprate.py
@@ -33,6 +33,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Parse show datapath session dpi by the bitrate of each session")
     parser.add_argument('infile', help="Input file(s)", type=str, nargs='+')
+    parser.add_argument('--pattern', '-p', help='regex for AP name', type=str, default='.*')
+    parser.add_argument('--top', '-t', help='Top N sessions', type=int, default=200)
     parser.add_argument('--debug', help='Enable debug log', action='store_true')
     args = parser.parse_args()
 
@@ -63,11 +65,12 @@ if __name__ == '__main__':
     #   parse user-table and create IP to AP Name mapping
     #
     ip2apn = {}
+    maxlen = 0
     for r in user_tbl[1:]:
         ip = r[0]
         apn = r[7]
         ip2apn[ip] = apn
-
+        maxlen = max(maxlen, len(apn))
 
     #
     #   get top 200 high-bandwidth session
@@ -85,17 +88,23 @@ if __name__ == '__main__':
         tage = int(r[idx_tage], 16)
         if tage <= 5: continue          # ignore short-lived session
         if r[2] == '47': continue       # ignore GRE tunnel
+        sap = ip2apn.get(r[0], '')
+        dap = ip2apn.get(r[1], '')
+        if (not re.search(args.pattern, sap)) and (not re.search(args.pattern, dap)):
+            continue        # AP name does not match
+
         bytes = int(r[idx_bytes])
         flags = r[idx_flags]
         bitrate = bytes*8/tage
         tot_br += bitrate
-        tbl.append([r[0], r[1], r[2], r[3], r[4], r[idx_tos], tage, bytes, bitrate, flags, r[idx_appid][:16].rstrip()])
+        # SIP, SAP, DIP, DAP, Proto, SPort, DPort, ToS, TAge, Bytes, Bitrate, Flags, AppID
+        tbl.append([r[0], sap, r[1], dap, r[2], r[3], r[4], r[idx_tos], tage, bytes, bitrate, flags, r[idx_appid][:16].rstrip()])
 
         m = re.match(r'(\d+)\.', r[1])
         if m:
             ip = int(m.group(1))
             if 224 <= ip <= 239:
-                tbl_m.append([r[0], r[1], r[2], r[3], r[4], tage, bytes, bitrate, flags])
+                tbl_m.append([r[0], sap, r[1], r[2], r[3], r[4], tage, bytes, bitrate, flags])
 
         if 'V' in flags: num_v += 1
         if 'I' in flags: num_i += 1
@@ -104,25 +113,20 @@ if __name__ == '__main__':
 
     i = 1
     #print('Src,Dst,Proto,SPort,DPort,Bytes,Bitrate(Kbps),Flags')
-    print("Src IP              Src AP              Dst IP              Dst AP              Proto  SPort  DPort  ToS  Bytes        Dur   BW(Kbps)  Flags  AppID")
-    print("------              ------              ------              ------              -----  -----  -----  ---  -----        ---   --------  -----  -----")
-    for r in sorted(tbl, key=lambda x:x[8], reverse=True)[:200]:
-        src = r[0]
-        dst = r[1]
-        sap = ip2apn.get(src, '')
-        dap = ip2apn.get(dst, '')
-        print(f'{src:20}{sap:20}{dst:20}{dap:20}{r[2]:7}{r[3]:7}{r[4]:7}{r[5]:3}  {r[7]:>10}{r[6]:>6} {r[8]/1000:>10.2f}  {r[9]:5}  {r[10]}')
-        #print(f'{src},{dst},{r[2]},{r[3]},{r[4]},{r[6]},{r[7]/1000:>8.2f},{r[8]}')
+    spc = " " * (maxlen-4)
+    print(f"Src IP              Src AP{spc}Dst IP              Dst AP{spc}Proto  SPort  DPort  ToS  Bytes        Dur   BW(Kbps)  Flags  AppID")
+    print(f"------              ------{spc}------              ------{spc}-----  -----  -----  ---  -----        ---   --------  -----  -----")
+    # Sorty by BW
+    for r in sorted(tbl, key=lambda x:x[10], reverse=True)[:args.top]:
+        print(f'{r[0]:20}{r[1]:{maxlen+2}}{r[2]:20}{r[3]:{maxlen+2}}{r[4]:7}{r[5]:7}{r[6]:7}{r[7]:3}  {r[9]:>10}{r[8]:>6} {r[10]/1000:>10.2f}  {r[11]:5}  {r[12]}')
         i+=1
 
     print(f"Total bitrate: {tot_br/1000/1000:.2f} Mbps")
     #sys.exit(0)
 
     print("\n==== Multicast ====")
-    for r in sorted(tbl_m, key=lambda x:x[7], reverse=True)[:100]:
-        src = r[0]
-        dst = r[1]
-        print(f'{i:>3}: {src:20}{dst:20}{r[2]:5}{r[3]:6}{r[4]:6}{r[6]:>10} {r[7]/1000:>8.2f} Kbps')
+    for r in sorted(tbl_m, key=lambda x:x[8], reverse=True)[:100]:
+        print(f'{i:>3}: {r[0]:20}{r[1]:20}{r[2]:20}{r[3]:5}{r[4]:6}{r[5]:6}{r[7]:>10} {r[8]/1000:>8.2f} Kbps')
         #print(f'{ip},{r[1]},{ip2name[ip]},{ip2apn[ip]},{ip2ssid[ip]},{r[2]}')
         i+=1
 
