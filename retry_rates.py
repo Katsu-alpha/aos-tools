51af1099aabfdb5a0191ed96f92f97243f85f811
Katsuhiro Sasaki <git@k2hiro.com>
2026-02-21 23:20:11 +0800

    add overall retry rates if multiple client-tables are found

--- retry_rates.py
+++ retry_rates.py
@@ -1,5 +1,6 @@
 #
 #   show ap debug client-table の Tx Retry 率を計算
+#   同じ AP に対して複数回取得された場合は、AP 全体のリトライ率を計算
 #
 
 import sys
@@ -14,8 +15,8 @@ TX_PKTS_THRESHOLD = 4000      # Tx 4,000パケット未満の端末は除外
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description="convert 'show ap database long' output to MS Excel")
-    parser.add_argument('files', type=str, nargs='*')
+        description="calculate Tx retry rate for show ap debug client-table outputs")
+    parser.add_argument('files', type=str, nargs='+')
     parser.add_argument('--debug', help='debug log', action='store_true')
     args = parser.parse_args()
 
@@ -36,6 +37,10 @@ if __name__ == '__main__':
     max_tx_pkts = {}
     rows = {}
     retry_rate = {}
+    sta_txpkts_st = {}
+    sta_txretr_st = {}
+    sta_txpkts_ed = {}
+    sta_txretr_ed = {}
 
     for tbl in tbls:
         idx_tx_pkts = tbl[0].index("Tx_Pkts")
@@ -47,6 +52,14 @@ if __name__ == '__main__':
             mac = row[0]
             tx_pkts = int(row[idx_tx_pkts])
             tx_retr = int(row[idx_tx_retr])
+
+            # per-STA Tx/Retr values
+            sta_txpkts_ed[mac] = tx_pkts
+            sta_txretr_ed[mac] = tx_retr
+            if mac not in max_tx_pkts:
+                sta_txpkts_st[mac] = tx_pkts
+                sta_txretr_st[mac] = tx_retr
+
             if tx_pkts < TX_PKTS_THRESHOLD:
                 continue
             try:
@@ -75,5 +88,25 @@ if __name__ == '__main__':
             print(f"{mac:20}{tx_pkts:10}{tx_retr:10}  {txrate:6}   {snr:5}{retr_rate:4.2f}")
 
         print("")
+
+    #   per-STA retry rate
+    tot_pkts = 0
+    tot_retr = 0
+    for mac in sta_txpkts_ed.keys():
+        tx_pkts_st = sta_txpkts_st[mac]
+        tx_retr_st = sta_txretr_st[mac]
+        tx_pkts_ed = sta_txpkts_ed[mac]
+        tx_retr_ed = sta_txretr_ed[mac]
+        delta_pkts = tx_pkts_ed - tx_pkts_st
+        delta_retr = tx_retr_ed - tx_retr_st
+        tot_pkts += delta_pkts
+        tot_retr += delta_retr
+        retr_rate = delta_retr / delta_pkts * 100 if delta_pkts > 0 else 0.0
+        print(f"{mac}: st: {tx_retr_st}/{tx_pkts_st}, ed: {tx_retr_ed}/{tx_pkts_ed}, delta: {delta_retr}/{delta_pkts}, Retry%: {retr_rate:.2f}%")
+
+    #   overall retry rate
+    if tot_pkts > 0:
+        rr = tot_retr / tot_pkts * 100
+        print(f"Retry rate on AP: {rr:.2f}% ( {tot_retr} / {tot_pkts} )")
 
     sys.exit(0)
