1f8d500d2824f131d926e6e5a98b9875a41094d4
Katsuhiro Sasaki <k2hiro@gmail.com>
2026-07-20 23:00:10 +0800

    add --summary and --coch options

--- ap-monitor-list.py
+++ ap-monitor-list.py
@@ -96,6 +96,32 @@ def isIntf(ch1, ch2):
         return True
     
     return False
+
+
+def print_table(tbl):
+    maxlen = [0] * (len(tbl[0])-1)
+    for r in tbl:
+        for i, c in enumerate(r[1:]):
+            if len(str(c)) > maxlen[i]:
+                maxlen[i] = len(str(c))
+
+    sep = [None]
+    for c in tbl[0][1:]:
+        sep.append('-' * len(c))
+    tbl.insert(1, sep)
+
+
+    for r in tbl:
+        l = ""
+        for i, c in enumerate(r[1:]):
+            l += f"{c:<{maxlen[i]+2}}"
+        if r[0]:
+            l = r[0] + l + RESET
+        print(l)
+
+
+
+
 #
 #   main
 #
@@ -108,6 +134,8 @@ if __name__ == '__main__':
     parser.add_argument('--debug', help='Enable debug log', action='store_true')
     parser.add_argument('--band', '-b', help='Radio band', type=str, default='5')
     parser.add_argument('--summary', help='Summary only', action='store_true')
+    parser.add_argument('--coch', help='Co-channel APs only', action='store_true')
+    parser.add_argument('--group', help='Group adjacent BSSIDs for interfering APs', action='store_true')
     parser.add_argument('--bssdic', '-d', help='Specify BSSID dictionary', type=str)
     args = parser.parse_args()
 
@@ -189,89 +217,104 @@ if __name__ == '__main__':
             print("No APs found.")
             sys.exit(0)
 
+
+
+        valid_aps = [r for r in aplist if r[6] == "valid"]              # Valid AP list
+        intf_aps = [r for r in aplist if r[6] != "valid" and r[8]!=0]   # non-Valid and SNR!=0 AP list
+
         #
-        #   結果表示
+        #   Display Valid APs
         #
         bss_dedup = set()
-        aplist.sort(key=lambda x: x[8], reverse=True)
-        rslt = [
-            "****************** Valid APs ******************",
-            "",
-            "                    BSSID               ESSID    Chan     CBW/PHY      Type               Enc   SNR  RSSI  AP Name",
-            "                    -----               -----    ----     -------      ----               ---   ---  ----  -------",
-            ]
+        valid_aps.sort(key=lambda x: x[8], reverse=True)    # sort by curr-snr
+        rslt = [[None, "BSSID", "ESSID", "Chan", "CBW/PHY", "Type", "Enc", "SNR", "RSSI", "AP Name"]]
+        rslt_coch = [[None, "BSSID", "AP Name", "Chan", "SNR"]]
 
         valid_tot = 0
         valid_coch_snr10 = 0
         cov_ap = 0      # Valid APs with SNR >= 30
-        for r in aplist:
+        for r in valid_aps:
             bss, ap_type, pch = r[0], r[6], r[3]
-            if ap_type == "valid":
-                if bss[:16] in bss_dedup:
-                    continue
-                bss_dedup.add(bss[:16])
-                cbw_phy = r[4] + '/' + r[5]
-                apn = bss2apn.get(bss[:17], '')
-                ch = r[2]
-                snr = r[8]
-                valid_tot += 1
-                l = f"{valid_tot:>3}{bss:>22}{r[1]:>20}{r[2]:>8}{cbw_phy:>12}{r[6]:>10}{r[7]:>18}{r[8]:>6}{-r[9]:>6}  {apn}"
+            if bss[:16] in bss_dedup:
+                continue
+            bss_dedup.add(bss[:16])
+            cbw_phy = r[4] + '/' + r[5]
+            apn = bss2apn.get(bss[:17], '')
+            ch = r[2]
+            snr = r[8]
+            valid_tot += 1
+            rr = [None, bss, r[1], r[2], cbw_phy, r[6], r[7], r[8], -r[9], apn]
 
-                if bss.endswith('(+)'):
-                    rslt.append(GREEN + l + RESET)
-                else:
-                    if snr >= 30:
-                        cov_ap += 1
-                    if isIntf(ch, mych):
-                        if snr >= 10:
-                            rslt.append(RED + l + RESET)
-                            valid_coch_snr10 += 1
-                        else:
-                            rslt.append(YELLOW + l + RESET)
+            if bss.endswith('(+)'):
+                rr[0] = GREEN
+            else:
+                if snr >= 30:
+                    cov_ap += 1
+                if isIntf(ch, mych):
+                    if snr >= 10:
+                        rr[0] = RED
+                        rslt_coch.append([None, bss, apn, ch, snr])
+                        valid_coch_snr10 += 1
                     else:
-                        rslt.append(l)
-
-        if not args.summary:
-            print("\n".join(rslt))
-            print(f"\nTotal Valid APs: {valid_tot}, Coverage APs with SNR>=30: {cov_ap}, Co-ch APs with SNR>=10: {valid_coch_snr10}\n")
+                        rr[0] = YELLOW
+            rslt.append(rr)
 
         if args.summary:
             # print(f"{myapn}: Channel:{mych}  Coverage APs:{cov_ap}  Co-ch APs:{valid_coch_snr10}")
             print(f'"{myapn}","{mych}",{cov_ap},{valid_coch_snr10}')
             continue
+        elif args.coch:
+            print("****************** Valid Co-channel APs ******************\n")
+            print_table(rslt_coch)
+        else:
+            print("****************** Valid APs ******************\n")
+            print_table(rslt)
+            print(f"\nTotal Valid APs: {valid_tot}, Coverage APs with SNR>=30: {cov_ap}, Co-ch APs with SNR>=10: {valid_coch_snr10}\n")
 
-        rslt = [
-            f"****************** Interfering APs seen by {myapn} ******************",
-            "",
-            "                    BSSID                    ESSID    Chan     CBW/PHY                  Type               Enc   SNR  RSSI",
-            "                    -----                    -----    ----     -------                  ----               ---   ---  ----",
-            ]
+
+        #
+        #   Display Interfering APs
+        #
+        rslt = [[None, "BSSID", "ESSID", "Chan", "CBW/PHY", "Type", "Enc", "SNR", "RSSI", "AP Name"]]
+        rslt_coch = [[None, "BSSID", "ESSID", "Chan", "Type", "SNR"]]
+
+        if args.group:
+            ap_snr = {}
+            for r in intf_aps:
+                bss, snr = r[0], r[8]
+                if bss[:16] not in ap_snr:
+                    ap_snr[bss[:16]] = snr
+            intf_aps.sort(key=lambda x: (-ap_snr[x[0][:16]], x[0]))    # sort by curr-snr, bssid
+        else:
+            intf_aps.sort(key=lambda x: x[8], reverse=True)    # sort by curr-snr
 
         intf_tot = 0
         intf_coch_snr10 = 0
-        for r in aplist:
-            bss, ap_type, pch = r[0], r[6], r[3]
-            if ap_type != "valid":
-                cbw_phy = r[4] + '/' + r[5]
-                apn = bss2apn.get(bss[:17], '')
-                ch  = r[2]
-                snr = r[8]
-                intf_tot += 1
-                l = f"{intf_tot:>3}{r[0]:>22}{r[1]:>25}{r[2]:>8}{cbw_phy:>12}{r[6]:>22}{r[7]:>18}{r[8]:>6}{-r[9]:>6}  {apn}"
+        for r in intf_aps:
+            bss, ess, ch, ap_type, enc, snr, rssi = r[0], r[1], r[2], r[6], r[7], r[8], r[9]
+            cbw_phy = r[4] + '/' + r[5]
+            apn = bss2apn.get(bss[:17], '')
+            intf_tot += 1
+            rr = [None, bss, ess, ch, cbw_phy, ap_type, enc, snr, -rssi, apn]
 
-                if isIntf(ch, mych):
-                    if snr >= 10:
-                        rslt.append(RED + l + RESET)
-                        intf_coch_snr10 += 1
-                    else:
-                        rslt.append(YELLOW + l + RESET)
+            if isIntf(ch, mych):
+                if snr >= 10:
+                    rr[0] = RED
+                    rslt_coch.append([None, bss, ess, ch, ap_type, snr])
+                    intf_coch_snr10 += 1
                 else:
-                    rslt.append(l)
+                    rr[0] = YELLOW
+            rslt.append(rr)
 
-        if not args.summary:
-            print("\n".join(rslt))
+
+        if args.coch:
+            print(f"\n****************** Interfering Co-channel APs seen by {myapn} on ch{mych} ******************\n")
+            print_table(rslt_coch)
+        elif not args.summary:
+            print(f"\n****************** Interfering APs seen by {myapn} on ch{mych} ******************\n")
+            print_table(rslt)
             print(f"\nTotal Interfering APs: {intf_tot}, Co-ch APs with SNR>=10: {intf_coch_snr10}\n")
-
+            
 
 
 
